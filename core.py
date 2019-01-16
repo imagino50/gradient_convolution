7351007f8f286ca0202cc4d11e9248617cae780a
Arnaud <arnaud.paumard@laposte.net>
2019-01-16 19:57:16 +0100

    Update core.py

--- core.py
+++ core.py
@@ -260,19 +260,4 @@ def convolve(image, Gm, Gd, stride=1, th=1.0):
     return feature_maps
             
          
-            
-            
-plt.figure("SOBEL EDGES DETECTION") 
-plt.subplot(1,4,1) 
-plt.imshow(image, interpolation='nearest') 
-plt.title("IMAGE") 
-plt.subplot(1,4,2) 
-plt.imshow(edge_magnitues, interpolation='nearest') 
-plt.title("MAGNITUDES") 
-plt.subplot(1,4,3) 
-plt.imshow(edge_angles, interpolation='nearest') 
-plt.title("ANGLES") 
-plt.subplot(1,4,4) 
-plt.imshow(edge_angles2, interpolation='nearest') 
-plt.title("ANGLES THRES") 
-plt.show()
+
